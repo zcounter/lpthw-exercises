37b535f2b0a49356684924fad8ccebad0dd6e8f4
zcounter <michael.quintero@gmail.com>
2014-03-15 13:30:08 -0500

    Some changes to ex7

--- ex7.py
+++ ex7.py
@@ -1,6 +1,13 @@
+# This line will print the line below
 print "Mary had a little lamb."
+
+# This line will print the line below, as well as populate the given string
 print "Its fleece was white as %s." % 'snow'
+
+# This line will print the line below
 print "And everywhere that Mary went."
+
+# This line will print the decimal character 10 times, with a comment behind the 
 print "." * 10 # what'd that do?
 
 end1 = "C"
@@ -16,6 +23,7 @@ end10 = "g"
 end11 = "e"
 end12 = "r"
 
-# Watch that comma at the end. try removing it to see what happens
+# If you remove the comment at the end of the 1st phrase, the entire phrase will be split. All we are doing here is adding
+# the values of each string variable together to give the complete phrase, Cheeseburger
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
