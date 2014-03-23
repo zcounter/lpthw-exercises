a09554b41a7d9eb26bef281b4b2a99affaa62da4
zcounter <michael.quintero@gmail.com>
2014-03-23 14:39:14 -0500

    Exercise 10, "What was that?

--- ex10.py
+++ ex10.py
@@ -13,3 +13,26 @@ print tabby_cat
 print persian_cat
 print backslash_cat
 print fat_cat
+
+""" 
+Was asked to enter in the following code:
+while True:
+    for i in ["/","-","|","\\",","|"]:
+        print "%s\r" % i,
+
+What this code did was creating a spinning slash, that could be used when waiting for a process to complete. We will revisit this later.
+
+
+Tasks:
+
+1. Memorize all escape sequences by putting them on flash cards
+
+2. Use ''' (triple-single-quote) instead. Can you see what you might use that instead of \"\"\"?
+
+3. Combine escape sequences and format strings to create a more complex format.
+
+4. Remember the %r format? Combine %r with double-quote and single-quote escapes and prints them out. Compare %r with %s. Notice how %r prints it the way you'd write it in your file, but %s prints it the way you'd like to see it?
+
+"""
+
+
